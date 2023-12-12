f658a02a9a21e4ef75e404623baf0f7c9e18cddf
HarshaVardhan <hunterharsha007@gmail.com>
2023-12-12 09:19:21 +0530

    new changes

--- search.js
+++ search.js
@@ -1,9 +1,2 @@
 // Hamburger
-
-function toggleNav() {
-    const nav = document.getElementById('search-input');
-    const bars = document.querySelectorAll('.bar');
-
-    search-input.classList.toggle('change');
-    bars.forEach(bar => bar.classList.toggle('change'));
-}
+document.getElementById("search-input").style.fontFamily="Berkshire Swash";
